0e64043b4367a66993003f9d0ca8142fc0483292
OTP <c24534-34dh43d5-d345j@google.com>
2021-02-10 00:54:42 +0200

    PEP8 Lint

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import requests
-import zipfile
 import random
+import zipfile
+
+import requests
+
 
 # takes the file (a folder named "pp", and feeds it to this function) - As for multiple hosts, a lot of these sites have near
 # identical APIS, so it basically just looks like this. for "all" of them.
@@ -8,29 +10,35 @@ def upload_file(f):
     try:
         upload = requests.post("https://api.bayfiles.com/upload", files=f)
         if upload.status_code == 200:
-           return upload.content
+            return upload.content
     except Exception as e:
         print(e)
 
-#fetches animal list from GitHub, puts two animals together
+
+# fetches animal list from GitHub, puts two animals together
 def get_wordlist():
-    fetch_words = requests.get("https://gist.githubusercontent.com/mbauer14/eaaa001b7fb8073dd576/raw/84501d87e7ac3a134700862a6b22916c9cb16773/animals.txt")
+    fetch_words = requests.get(
+        "https://gist.githubusercontent.com/mbauer14/eaaa001b7fb8073dd576/raw/84501d87e7ac3a134700862a6b22916c9cb16773/animals.txt")
     return fetch_words.text.splitlines()
+
 
 # takes the animals, puts them in an array and chooses one at random.
 def create_name():
     word = ([c.strip() for c in get_wordlist()])
-    return  random.choice(word) + random.choice(word) + ".zip"
+    return random.choice(word) + random.choice(word) + ".zip"
+
+
 archive_name = create_name()
 
 
 def create_archive():
     try:
-     archive = zipfile.ZipFile(archive_name, 'w')
-     archive.write('pp')
-     archive.close()
+        archive = zipfile.ZipFile(archive_name, 'w')
+        archive.write('pp')
+        archive.close()
     except:
         print("Error?")
+
 
 # was going to make a Main Function but indentation gives me motion sickness
 create_archive()
@@ -43,4 +51,3 @@ try:
 
 except Exception as e:
     print(e)
-
