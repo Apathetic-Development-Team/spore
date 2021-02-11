a513788869d0406ebc30f87a41fb226b65747080
OTP <c24534-34dh43d5-d345j@google.com>
2021-02-11 02:34:23 +0200

    split file hosts into functions

--- upload.py
+++ upload.py
@@ -1,12 +1,56 @@
+import json
+
 import requests
 
+
+def bayfiles_upload(files):
+    try:
+        r = requests.post("https://api.bayfiles.com/upload", files=files)
+        resp = json.loads(r.content)
+        print(resp['data']['file']['url']['short'])
+        if r.status_code == 200:
+            print('BayFiles Uploaded.')
+        else:
+            print('Something else happened')
+    except Exception as e:
+        print('Bayfiles did not upload')
+
+
+def anonfiles_upload(files):
+    try:
+        r = requests.post("https://api.anonfiles.com/upload", files=files)
+        resp = json.loads(r.content)
+        print(resp['data']['file']['url']['short'])
+        if r.status_code == 200:
+            print('AnonFiles Uploaded.')
+        else:
+            print('Something else happened')
+    except Exception as e:
+        print('AnonFiles did not upload')
+
+
+def gofile_upload(files):
+    try:
+        r = requests.post("https://srv-store1.gofile.io/uploadfile", files=files)
+        resp = json.loads(r.content)
+        print('https://gofile.io/d/' + resp['data']['code'])
+        if r.status_code == 200:
+            print('GoFile Uploaded.')
+        else:
+            print('Something else happened')
+    except Exception as e:
+        print(e)
+
+
 def push(archive_name):
-    files = {'file': open(archive_name, 'rb')}
 
     try:
-        upload = requests.post("https://api.bayfiles.com/upload", files=files)
-        print(upload.content)
+        with open(archive_name, 'rb') as archive:
+            files = {'file': (archive.name, archive.read())}
+            bayfiles_upload(files)
+            anonfiles_upload(files)
+            gofile_upload(files)
     except:
-        print("File failed to Upload.")
+        print("Failed to Upload (all?) files")
 
     return 0
