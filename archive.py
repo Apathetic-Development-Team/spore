1a8710158fab1b92a094decb75e34ad6f3149a56
wwhtrbbtt <64318024+wwhtrbbtt@users.noreply.github.com>
2021-02-17 23:56:07 +0100

    Now uses 7z with a password
    I completely rewrote archive.py. It now creates a 7zip archive using py7zr with the password raidforums. In that archive, an `info.md` file is "injected". It contains credits and a shoutout to raidforums, Omni requested it. Also, I've added some more words to the name generator.

--- archive.py
+++ archive.py
@@ -1,22 +1,56 @@
-import random
-import zipfile
-import upload
+from random import choice
+from os import mkdir
+from upload import is_gui, push
+from shutil import copy, rmtree
+import py7zr
 
-words = ["milk", "eggs", "sausage", "bacon", "hotdog"]
+PASSWORD = "raidforums"
+words = ["milk", "eggs", "sausage", "bacon", "hotdog", "muffin", "banana", "apple", "noodles"]
 
+included_file = "\n\n".join([
+    "# AUTHORS",
+    "https://github.com/Apathetic-Development-Team/spore",
+    "CYBERLUST: https://github.com/CYBERLUST",
+    "Forsol: https://github.com/Forsol",
+    "whtrbbt: https://github.com/wwhtrbbtt",
+    "# SHOUTOUT" ,
+    "Huge shoutout to https://raidforums.com, especially the owner, OmniPotent."
+])
 
-def process(user_input):
-    archive_name = random.choice(words) + random.choice(words) + ".zip"
-    archive = zipfile.ZipFile(archive_name, 'w')
+def create_folder(user_input):
+    print("Copying all files in a temp folder...")
+
+    # make an empty folder called temp_folder
+    rmtree('temp_folder', ignore_errors=True)
+    mkdir("temp_folder")
+
+    with open("temp_folder/info.md", "w") as f:
+        f.write(included_file)
+
+    # if the user wants multiple files, iterate over them
     if type(user_input) is tuple:
-        upload.is_gui(True)
-        for i in user_input:
-            print(i)
-            archive.write(i, i.rsplit('/', 1)[-1])
-    else:
-        upload.is_gui(False)
-        archive.write(user_input, user_input.rsplit('/', 1)[-1])
-    archive.close()
+        is_gui(True)
+        for file in user_input:
+            copy(file, "temp_folder/" + file.split("/")[-1])
 
+    # if they want only 1 file
+    else:
+        is_gui(False)
+        copy(user_input, "temp_folder/" + user_input.split("/")[-1])
+
+    print("Done!")
+    
+def process(user_input):
+    create_folder(user_input)
+
+    # generate the archive name
+    archive_name  = "".join([choice(words) for _ in range(2)]) + ".7z"
+
+    # create a 7z archive with the specified password
+    with py7zr.SevenZipFile(archive_name, 'w', password=PASSWORD) as archive:
+        archive.writeall('temp_folder', '/')
+    
     print(archive_name + " generated sucessfully. Uploading now... ")
-    upload.push(archive_name)
+    # upload and delete it.
+    push(archive_name)
+    rmtree('temp_folder', ignore_errors=True)
