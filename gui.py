bbdd3d12a4fc5708d91e549c0a41ceac485d2e3e
wwhtrbbtt <64318024+wwhtrbbtt@users.noreply.github.com>
2021-02-17 17:45:14 +0100

    Add files via upload

--- gui.py
+++ gui.py
@@ -4,42 +4,57 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter.filedialog import askopenfilenames
 
-displayText = None
+VERSION = "2.1"
+GITHUB = "https://github.com/Apathetic-Development-Team/spore"
 
+displayText = None
 
 def uploadaction(event=None):
     filename = askopenfilenames()
     if filename:
         print('got file, sending to archive handler')
         print(filename)
-        # messagebox.showinfo(title="Spore",
-        #                     message="The GUI will not work while uploading, please refer to the CLI for output")
         archive.process(filename)
     else:
-        messagebox.showinfo(title="Spore", message="You did not select a File/Folder")
+        messagebox.showinfo(title="Spore - error", message="You did not select a File/Folder")
 
 
 def infobox(msg):
-    messagebox.showinfo(title="Spore", message=msg)
+    messagebox.showinfo(title="Spore - message", message=msg)
 
 
 def init():
     root = Tk()
     root.geometry("400x245")
-    root.title("Spore v0.1")
+    root.title(f"🦠 Spore v{VERSION} 🦠")
 
-    thislabel = Label(root, text="Spore super cool file uploading")
-    thislabel.pack()
-    uploadbutton = Button(root, text="Upload", command=uploadaction)
+    # the icon
+    photo = PhotoImage(file="images/icon.png")
+    root.iconphoto(False, photo)
+
+    # the label at the top
+    header = Label(root, text="Spore - file uploading made easy", bg="black", fg="white", font=('Verdana', 20),)
+    header.pack(fill=X, pady=(0,15))
+    
+    # the button to browse a file and upload it
+    uploadbutton = Button(root, text="Upload file",font=('Verdana', 15), command=uploadaction, relief='flat')
     uploadbutton.pack()
 
+    # the URLs that will be displayed
     global displayText
     displayText = Text(root, height=10, width=50)
-    displayText.pack()
     displayText.configure(state='disabled')
+    displayText.pack()
+
+    # the label at the bottom
+    footer = Button(root, command=opengithub, text=f"Github: {GITHUB.replace('https://', '')}", bg="black", fg="black", font=('Verdana', 10))
+    footer.pack(fill=X, side=BOTTOM)
 
     root.mainloop()
 
+def opengithub():
+    from webbrowser import open
+    open(GITHUB, new=2)
 
 def update_links(msg):
     displayText.configure(state='normal')
